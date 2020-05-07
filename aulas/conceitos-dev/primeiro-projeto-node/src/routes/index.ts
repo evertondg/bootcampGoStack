5879a48040d16fb64481fad55200c82eff074be5
evertondg <evertondg@hotmail.com.br>
2020-05-07 01:13:26 -0300

    feat: Finalizando módulo Construindo Aplicação

--- aulas/conceitos-dev/primeiro-projeto-node/src/routes/index.ts
+++ aulas/conceitos-dev/primeiro-projeto-node/src/routes/index.ts
@@ -1,18 +1,7 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
-
-// routes.get('/', (request, response) =>
-//   response.json({ message: 'Hello World!' }),
-// );
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
