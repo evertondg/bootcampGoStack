c254e23ff493dc01d6a1a49374a7108b624c1ff6
Everton De Grande <evertondg@hotmail.com.br>
2020-06-24 08:06:45 -0300

    feat: Conclusão do cadastro de repositórios do Github com React

--- aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,55 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import logoImage from '../../assets/logo.svg';
+import {Link} from 'react-router-dom'
 
-import { Title, Form, Repositories } from './style';
+import logoImage from '../../assets/logo.svg';
+import api from '../../services/api';
+
+import { Title, Form, Repositories, Error } from './style';
+
+interface Repository{
+  full_name: string;
+  description: string;
+  owner:{
+    login: string;
+    avatar_url: string;
+
+  }
+}
+
 
 const Dashboard: React.FC = () => {
+
+  const [newRepo,setNewRepo] = useState('');
+  const [inputError,setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(()=>{
+    const storageRepositories = localStorage.getItem('@GitHubExplorer:repositories');
+
+    if (storageRepositories){
+      return JSON.parse(storageRepositories)
+    }else{
+      return []
+    }
+  });
+
+  useEffect(()=>{
+    localStorage.setItem('@GitHubExplorer:repositories',JSON.stringify(repositories))
+  },[repositories])
+
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>):Promise<void>{
+    event.preventDefault();
+
+
+    if (!newRepo){
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories,repository]);
+        setNewRepo('');
+        setInputError('');
+
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImage} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form action="" hasError={!!inputError} onSubmit={handleAddRepository} >
+        <input placeholder="Digite o nome do repositório" value={newRepo} onChange={ e => setNewRepo(e.target.value)}/>
         <button type="submit">Pesquisar</button>
       </Form>
-
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/2487230?s=460&u=55e988a5c39e6437c6e292c38685b8ef5f11f032&v=4"
-            alt="Everton De Grande"
-          />
-          <div>
-            <strong>evertondg/doom-fire-algorithm</strong>
-            <p>Desenvolvimento do algoritmo do fogo estilo "DooM"</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
 
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/2487230?s=460&u=55e988a5c39e6437c6e292c38685b8ef5f11f032&v=4"
-            alt="Everton De Grande"
-          />
-          <div>
-            <strong>evertondg/doom-fire-algorithm</strong>
-            <p>Desenvolvimento do algoritmo do fogo estilo "DooM"</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {
+          repositories.map(repository =>(
+            <Link  key={repository.full_name} to={`/repository/${repository.full_name}`}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.avatar_url}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+          ))
+        }
 
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/2487230?s=460&u=55e988a5c39e6437c6e292c38685b8ef5f11f032&v=4"
-            alt="Everton De Grande"
-          />
-          <div>
-            <strong>evertondg/doom-fire-algorithm</strong>
-            <p>Desenvolvimento do algoritmo do fogo estilo "DooM"</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+
+
       </Repositories>
     </>
   );
