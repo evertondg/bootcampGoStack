7554cc06f0cb1902bd6282751e817adc43efefa2
Everton De Grande <evertondg@hotmail.com.br>
2020-07-28 23:15:30 -0300

    feat: Criando rotas do Aplicativo GoBarber mobile

--- aulas/appgobarber/src/App.tsx
+++ aulas/appgobarber/src/App.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import {View,Text} from 'react-native'
+import 'react-native-gesture-handler';
 
-const App: React.FC = ()=> <View><Text>TESTE EVERTON</Text></View>
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
