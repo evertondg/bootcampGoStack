801d32e5595b2779f514fc7011a54fe543688bc5
Everton De Grande <evertondg@hotmail.com.br>
2020-08-12 15:55:50 -0300

    feat: Adicionado serviço de envio de emails via SES Amazon

--- aulas/iniciando-back-end/src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ aulas/iniciando-back-end/src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import { classToClass } from 'class-transformer';
 
 export default class UsersAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
@@ -11,6 +12,6 @@ export default class UsersAvatarController {
     });
 
     delete user.password;
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
