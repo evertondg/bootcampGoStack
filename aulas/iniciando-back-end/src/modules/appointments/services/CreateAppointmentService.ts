dc78cc2e0410f601b3c0645d7b38a5b6a43fed3b
Everton De Grande <evertondg@hotmail.com.br>
2020-08-12 23:53:26 -0300

    feat: Cache de Agendamentos do Prestador de serviços no dia

--- aulas/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ aulas/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,10 @@
 import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
+
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import Appointment from '../infra/typeorm/entities/Appointments';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -19,6 +22,9 @@ class CreateAppointmentService {
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -54,6 +60,13 @@ class CreateAppointmentService {
     });
 
     const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`,
+    );
 
     await this.notificationsRepository.create({
       recipient_id: provider_id,
--- aulas/iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ aulas/iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -29,18 +29,23 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
-    // const cacheData = await this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    const appointments = await this.appointmentRepository.findAllInDayFromProvider(
-      {
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
+    );
+
+    if (!appointments) {
+      appointments = await this.appointmentRepository.findAllInDayFromProvider({
         provider_id,
         day,
         month,
         year,
-      },
-    );
+      });
 
-    // await this.cacheProvider.save('asd', 'asd');
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
+
     return appointments;
   }
 }
