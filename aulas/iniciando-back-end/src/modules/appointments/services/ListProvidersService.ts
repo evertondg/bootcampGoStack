04de9ca708f79109eaa29910781cb4407405c138
Everton De Grande <evertondg@hotmail.com.br>
2020-08-15 13:56:57 -0300

    feat: Adequaçao na API para serializar o retorno da lista de prestadores de serviço

--- aulas/iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
+++ aulas/iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -30,7 +31,10 @@ class ListProfileService {
       });
       console.log('A query no banco foi feita');
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
