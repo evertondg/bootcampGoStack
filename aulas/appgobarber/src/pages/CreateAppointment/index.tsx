6ec25ba27e1cbbb8825e16c7035251389029f21c
Everton De Grande <evertondg@hotmail.com.br>
2020-08-15 17:58:13 -0300

    feat: Estrutura de Criaçao de agendamentos

--- aulas/appgobarber/src/pages/CreateAppointment/index.tsx
+++ aulas/appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  const route = useRoute();
+  const routeParams = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
   return (
-    <View>
-      <Text>CreateAppointment</Text>
-    </View>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
   );
 };
 
